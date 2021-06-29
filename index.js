8f796ccebea1ccb1f92049c3c34b2fb9b4707af3
cecilia <leececilia14@gmail.com>
2021-06-30 07:29:50 +0900

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    if (pickup > 42) {
+        return (pickup-42);
+    }
+    else {
+       return (42-pickup);
+   }
+}
+
+function distanceFromHqInFeet(pickup) {
+    return distanceFromHqInBlocks(pickup)*264;
+}
+
+function distanceTravelledInFeet(start,destination) {
+    if (start>destination) {
+        return (start-destination)*264;
+    }
+    else {
+        return (destination-start)*264;
+    }
+}
+
+function calculatesFarePrice(start,destination) {
+    const distance= distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+      return 0;
+  }
+
+  else if (distance>400 && distance<=2000) {
+      return 0.02*(distance-400);
+  }
+  
+  else if (distance >= 2000 && distance<2500) {
+      return 25;
+  }
+
+  else {
+      return 'cannot travel that far';
+  }
+ 
+
+}
